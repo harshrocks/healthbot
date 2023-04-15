0e1278f3888d881bb97b1a00eb69eb1f960423c3
harshrocks <69761121+harshrocks@users.noreply.github.com>
2023-04-15 09:51:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import os
 from try2 import get_dataset_response, get_category_and_rating
 app = Flask(__name__)
 
-print("in flask app")
 @app.route('/', methods=['GET', 'POST'])
 def chatbot():
     return render_template('base.html')
@@ -13,17 +12,11 @@ def chatbot():
 def predict():
     try:
         text = request.get_json().get('message')
-        print(text)
         respo = get_dataset_response(text)
-        print(respo)
         response = respo[0]
-        print(response)
         closest_question = respo[1]
-        print(closest_question)
         message = {'answer': response}
-        print(message)
         if text.lower() in ["end", "stop", "quit", "exit", "bye"]:
-            print('in if')
             category = get_category_and_rating(response, closest_question)
 
             message = {'answer': f"Thank you for chatting with me!. I hope I was able to help you with your {category}."}
@@ -31,7 +24,6 @@ def predict():
         else:
             return jsonify(message)
     except Exception as e:
-        print(e)
         return jsonify({'answer': e})
 
 
