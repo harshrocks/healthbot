fab6cc18a1808d828b1c4232e813035f3f6daed2
harshrocks <69761121+harshrocks@users.noreply.github.com>
2023-04-15 08:42:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import os
 from try2 import get_dataset_response, get_category_and_rating
 app = Flask(__name__)
 
-
+print("in flask app")
 @app.route('/', methods=['GET', 'POST'])
 def chatbot():
     return render_template('base.html')
@@ -11,18 +11,30 @@ def chatbot():
 
 @app.post('/predict')
 def predict():
-    text = request.get_json().get('message')
-    respo = get_dataset_response(text)
-    response = respo[0]
-    closest_question = respo[1]
-    message = {'answer': response}
-    if text.lower() in ["end", "stop", "quit", "exit", "bye"]:
-        category = get_category_and_rating(response, closest_question)
-        message = {'answer': f"Thank you for chatting with me!. I hope I was able to help you with your {category}."}
-        return jsonify(message)
-    else:
-        return jsonify(message)
+    try:
+        text = request.get_json().get('message')
+        print(text)
+        respo = get_dataset_response(text)
+        print(respo)
+        response = respo[0]
+        print(response)
+        closest_question = respo[1]
+        print(closest_question)
+        message = {'answer': response}
+        print(message)
+        if text.lower() in ["end", "stop", "quit", "exit", "bye"]:
+            print('in if')
+            category = get_category_and_rating(response, closest_question)
+
+            message = {'answer': f"Thank you for chatting with me!. I hope I was able to help you with your {category}."}
+            return jsonify(message)
+        else:
+            return jsonify(message)
+    except Exception as e:
+        print(e)
+        return jsonify({'answer': e})
+
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False, host='0.0.0.0')
